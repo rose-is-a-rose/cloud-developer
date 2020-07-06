93af4ff4537bf1b189a61159693bffeb46dcf39e
Rose Zhang <rozzieka@gmail.com>
2020-07-06 11:55:15 -0400

    prettier formatting

--- course-02/project/image-filter-starter-code/src/routes/router.ts
+++ course-02/project/image-filter-starter-code/src/routes/router.ts
@@ -5,15 +5,15 @@ const router: Router = Router();
 
 // Root Endpoint
 // Displays a simple message to the user
-router.get( "/", async ( req: Request, res:Response ) => {
+router.get("/", async (req: Request, res: Response) => {
     res.send("try GET /filteredimage?image_url={{}}")
-} );
+});
 
 /**
  * GET /filteredimage?image_url={{URL}}
  */
 router.get('/filteredimage', async (req: Request, res: Response) => {
-    let {image_url} = req.query;
+    let { image_url } = req.query;
     // check image url is valid
     if (!image_url) {
         return res.status(400).send({ message: '`image_url` is required' });
